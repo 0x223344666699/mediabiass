5eb2439097cf70d90277042110c87e7e80543867
Juan J. Montero Muñoz <juan.mnt@gmail.com>
2018-04-15 23:13:30 +0200

    first commit

---
+++ gensim_news_optimized.py
@@ -0,0 +1,131 @@
+
+# coding: utf-8
+
+# ### TODOS 
+# #1 probar a comprobar todas las noticias vs. cada uno de los diccionarios
+
+# In[1]:
+
+import gensim
+import pandas as pd
+import logging
+from IPython.core.display import display, HTML 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+# In[2]:
+
+def clean_sentence(s):
+    return gensim.parsing.stem_text(gensim.parsing.strip_multiple_whitespaces(gensim.parsing.strip_non_alphanum(gensim.parsing.strip_short(gensim.parsing.remove_stopwords(str(s).lower().replace("á", "a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u")))))).split()
+
+
+# In[4]:
+
+news = pd.read_csv("df_news5.csv")
+
+# procesado de noticias
+periodicos = ["EM", "EP", "ABC"]
+ls = {}
+for p in periodicos:
+    if p == "EM": 
+        linkweb = "http://www.elmundo.es"
+    elif p == "EP":
+        linkweb = "http://www.elpais.com"
+    else:
+        linkweb = "http://www.abc.es"
+    titulares = list(news[news.periodico == linkweb].titular)
+    noticias = list(news[news.periodico == linkweb].noticia)
+    links = list(news[news.periodico == linkweb].link)
+    ls[p] = {"titulares":titulares, "noticias":noticias, "links":links, "docs":[], "linkweb":linkweb}
+    
+# limpia de textos + creacion del diccionario
+for p in periodicos:
+    for s in ls[p]['noticias']: ls[p]['docs'].append(clean_sentence(s))
+    ls[p]['dic'] = gensim.corpora.Dictionary(ls[p]['docs'])
+
+
+# In[5]:
+
+def build_todos(ls):
+    noticias_todos = []
+    for p in periodicos: noticias_todos += ls[p]['noticias']
+
+    docs_todos = []
+    for s in noticias_todos: docs_todos.append(clean_sentence(s))
+    dic_todos = gensim.corpora.Dictionary(docs_todos)
+    dic_todos.filter_n_most_frequent(10)
+    corpus = [dic_todos.doc2bow(doc) for doc in docs_todos]
+    gensim.corpora.MmCorpus.serialize("./corpus.mm", corpus)
+    return dic_todos
+
+dic_todos = build_todos(ls)
+corpus_todos = gensim.corpora.MmCorpus("./corpus.mm")
+
+
+# In[6]:
+
+base = "ABC"
+base2 = "EP"
+comp = "EM"
+
+dic_to_use = dic_todos #dic_wiki
+corpus_to_use = corpus_todos #corpus_wiki
+
+corpusbase = [dic_to_use.doc2bow(doc) for doc in ls[base]['docs']]
+gensim.corpora.MmCorpus.serialize("./corpusbase.mm", corpusbase)
+
+corpusbase2 = [dic_to_use.doc2bow(doc) for doc in ls[base2]['docs']]
+gensim.corpora.MmCorpus.serialize("./corpusbase2.mm", corpusbase2)
+
+corpusbase = gensim.corpora.MmCorpus("./corpusbase.mm")
+corpusbase2 = gensim.corpora.MmCorpus("./corpusbase2.mm")
+
+
+# In[7]:
+
+# build models
+tfidf = gensim.models.TfidfModel(corpus_to_use)
+lsi = gensim.models.LsiModel(tfidf[corpus_to_use], id2word=dic_to_use, num_topics=20)
+index = gensim.similarities.MatrixSimilarity(lsi[corpusbase], num_features=100) # transform corpus to LSI space and index it
+index2 = gensim.similarities.MatrixSimilarity(lsi[corpusbase2], num_features=100) # transform corpus to LSI space and index it
+
+
+# In[8]:
+
+## print resultados
+i = 0
+for d in ls[comp]['docs']:
+    vec_bow = dic_to_use.doc2bow(d)
+    vec_lsi = lsi[tfidf[vec_bow]]
+    sims = index[vec_lsi]
+    sims = list(enumerate(sims))
+    sims = sorted(sims, key=lambda t: -t[1])
+   
+    sims2 = index2[tfidf[vec_lsi]]
+    sims2 = list(enumerate(sims2))
+    sims2 = sorted(sims2, key=lambda t: -t[1])
+    
+    vec_bow = dic_to_use.doc2bow(ls[base]['docs'][sims[0][0]])
+    vec_lsi = lsi[tfidf[vec_bow]]
+    sims3 = index2[vec_lsi]
+    sims3 = list(enumerate(sims3))
+    sims3 = sorted(sims3, key=lambda t: -t[1])
+    
+    #if(sims[0][1] > 0.9) or (sims2[0][1] > 0.88) or (sims3[0][1] > 0.88):
+    print("---------- " + comp + " vs. " + base + ": " + str(sims[0][1])+" ----------" + comp + " vs. " + base2 + ": " + str(sims2[0][1]) + " ------------ " + base + " vs. " + base2 + ": " + str(sims3[0][1]))
+    display(HTML(str(i+1)+". "+comp+": " + ls[comp]['titulares'][i]+ ": <a href='" + ls[comp]['links'][i]+"'>Link</a>"))
+    if(sims[0][1] > 0.93): display(HTML(str(i+1)+". "+base+": "+ls[base]['titulares'][sims[0][0]]+ ": <a href='"+ls[base]['links'][sims[0][0]]+"'>Link</a>"))
+    if(sims2[0][1] > 0.88 and sims3[0][1] > 0.88) or sims2[0][1] > 0.93 or sims3[0][1] > 0.93: display(HTML(str(i+1)+". "+base2+": "+ls[base2]['titulares'][sims2[0][0]]+ ": <a href='"+ls[base2]['links'][sims2[0][0]]+"'>Link</a>"))
+
+    i += 1
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
---
+++ news_collector.py
@@ -0,0 +1,109 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+# In[ ]:
+
+newspapers = ["http://www.elmundo.es", "http://www.elconfidencial.com", "http://www.abc.es", "http://www.elpais.com"]
+dic_news = {"periodico":[], "titular":[], "noticia":[], "link":[]}
+categories_to_avoid = ["summum/estilo", "/verne", "/eventos", "/icon", "/cincodias", "/futbol", "/elcomidista", "/retina", "/tecnologia","/motor", "/loc/", "/television", "opinion", "deportes", "video", "galeria", "multimedia", "/loc/famosos", "/estilo/gente", "vanitatis", "cultura", "/play/", "metropoli", "ciencia-y-salud", "salud", "viajes", "plan-b", "/f5/", "/papel/", "/vida-sana/"]
+old_df_news = pd.DataFrame(dic_news)#pd.read_csv("df_news5.csv")
+for np in newspapers:
+    print("------ " + np + " -------")
+    r = requests.get(np).text
+    soup = BeautifulSoup(r, "html.parser")
+    count = 0
+
+    s = soup.find_all("h3", {"class":"mod-title"})
+    if np == "http://www.elconfidencial.com":
+        s = soup.find_all("h3", {"class":"art-tit"})
+    if np == "http://www.abc.es":
+        s = soup.find_all("h1", {"class":"titular"})
+    if np == "http://www.elpais.com":
+        s = soup.find_all("h2", {"class":"articulo-titulo"})
+
+    for t in s:
+        count += 1
+        txt = t.text
+        txt = txt.replace("\n", "")
+        if (t.a is not None):
+            #get link of article
+            href = t.a['href']
+            if (not any(cat in href for cat in categories_to_avoid)):
+                if ("http://" not in href) and ("https://" not in href):
+                    print("\t ADDING np: "+href)
+                    href = np+href
+
+                if href not in (old_df_news['link'].tolist()):
+                    rn = requests.get(href).text
+                    #parse article
+                    soup_news = BeautifulSoup(rn, "html.parser")
+                    ## elmundo.es
+                    divn = soup_news.find("div", {"itemprop":  "articleBody"})
+                    if np == "http://www.elconfidencial.com":
+                        divn = soup_news.find("div", {"id":  "news-body-center"})
+                    if np == "http://www.abc.es":
+                        divn = soup_news.find("span", {"class":  "cuerpo-texto"})
+                    if np == "http://www.pais.com":
+                        divn = soup_news.find("div", {"itemprop":  "articleBody"})
+                        
+                    if (divn is not None):
+                        noticia = ""
+                        for p in divn.find_all("p"):
+                            n = p.text
+                            if 'class' in p.attrs: print(n)
+                            if "ELMUNDO" not in n and "Síguenos en" not in n and 'class' not in p.attrs: noticia += " "+n
+                        if noticia != "":
+                            dic_news["periodico"].append(np)
+                            dic_news["titular"].append(txt)
+                            dic_news["noticia"].append(noticia)
+                            dic_news["link"].append(href)
+                            print("AÑADIENDO: " + href)
+                    else: 
+                        print("DIVN IS NONE: " + href)
+                else:
+                    print("YA PRESENTE: " + href)
+            else:
+                print("EVITADO: " + href)
+    print(count)
+
+
+# In[3]:
+
+df_news = pd.DataFrame(dic_news)
+print(old_df_news.shape)
+new_df_news = old_df_news.append(df_news, ignore_index=True)
+new_df_news.to_csv("df_news.csv", encoding="UTF-8", index=False)
+print(new_df_news.shape)
+
+
+# In[20]:
+
+import re
+new_df_news.noticia.apply(lambda x: print(re.findall("[a-zA-Z][a-zñ]*",str(x).replace("á", "a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u"))))
+
+
+# In[52]:
+
+new_df_news = pd.read_csv("df_news4.csv")
+print(new_df_news.shape)
+new_df_news = new_df_news[new_df_news['link'].apply(lambda x: "/xxx" not in x)]
+print(new_df_news.shape)
+
+
+# In[51]:
+
+new_df_news = new_df_news[["link", "noticia", "periodico", "titular"]]
+new_df_news.to_csv("df_news4.csv", encoding="UTF-8", index=False)
+
+
+# In[ ]:
+
+
+
